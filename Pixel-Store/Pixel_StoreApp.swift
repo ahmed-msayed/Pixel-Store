59972a1ae2646cc6c48385ccc06a19ba6ad030ec
Ahmed-msayed <ahmed.dev@icloud.com>
2023-12-07 23:34:31 +0200

    Initial Commit

---
+++ Pixel-Store/Pixel_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_StoreApp.swift
+//  Pixel-Store
+//
+//  Created by Ahmed Sayed on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
